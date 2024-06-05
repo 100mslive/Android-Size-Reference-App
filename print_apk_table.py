b240ebefed389b34e39c833086670f00e18e7413
Aniket Kadam <aniket.kadam@100ms.live>
2024-06-05 14:08:31 +0530

    Fix sizes

--- print_apk_table.py
+++ print_apk_table.py
@@ -2,7 +2,7 @@ import re
 import subprocess
 
 command = """find . -name '*.apk' | while read line; do
-     ls -l "$line" |  awk '{gsub(/\\.\\/app\\/build\\/outputs\\/apk\\//,"",$9);gsub(/.*release\\/app-[0-9][0-9]-/,"",$9);gsub(/-release-unsigned\\.apk/,"",$9);gsub(/0-without-sdk\\/release\\/app-0-/,"",$9);print}' | awk '{print $5, $9}'
+     ls -l "$line" |  awk '{gsub(/\\.\\/app\\/build\\/outputs\\/apk\\//,"",$9);gsub(/.*release\\/app-[0-9]/,"",$9);gsub(/-release-unsigned\\.apk/,"",$9);print}' | awk '{print $9, $5}'
 done"""
 
 ret = subprocess.run(command, capture_output=True, shell=True)
@@ -20,34 +20,44 @@ class TableSection:
 archMaps = dict()
 
 # The size of the apk without any 100ms libraries
-baseSize = -999999999
+baseSdkName = "Android Sdk"
+baseApkName = "Without Sdk"
+sizeInfo = text.splitlines()
+sizeInfo.sort()
 
-for line in text.splitlines():
+def removeNumber(line):
+	return line.split("-",1)[1]
+
+sizeInfo = map(removeNumber, sizeInfo)
+
+def subtractBy(libraryName, architecture):
+	if(libraryName == baseApkName):
+		return 0
+	elif(libraryName == baseSdkName):
+		return archMaps[baseApkName].x86
+	elif architecture == "x86":
+		return archMaps[baseSdkName].x86
+	elif architecture == "x86_64":
+		return archMaps[baseSdkName].x8664
+	elif architecture == "armeabi-v7a":
+		return archMaps[baseSdkName].armeabiv7a
+	elif architecture == "arm64-v8a":
+		return archMaps[baseSdkName].arm64v8a
+	else:
+		print("Missing arch {} is size {}".format(architecture, sizes))
+
+
+for line in sizeInfo:
 	l = line.split()
-	l[0] = int(l[0])
-	name = re.split('-(armeabi-v7a$|x86_64$|x86$|arm64-v8a$)',l[1])
+	l[1] = int(l[1])
+	name = re.split('-(armeabi-v7a$|x86_64$|x86$|arm64-v8a$)',l[0])
 	architecture = name[1]
 	libraryName = name[0].replace("-"," ").title()
-	if(libraryName == "Without Sdk"):
-		baseSize = l[0]
-		break
+	sizes = l[1]
+	if libraryName not in archMaps:
+		archMaps[libraryName] = TableSection()
+		archMaps[libraryName].name = libraryName
 
-for line in text.splitlines():
-	l = line.split()
-	l[0] = int(l[0]) - baseSize
-	name = re.split('-(armeabi-v7a$|x86_64$|x86$|arm64-v8a$)',l[1])
-	architecture = name[1]
-	libraryName = name[0].replace("-"," ").title()
-	archMaps[libraryName] = TableSection()
-	archMaps[libraryName].name = libraryName
-
-for line in text.splitlines():
-	l = line.split()
-	l[0] = int(l[0]) - baseSize
-	name = re.split('-(armeabi-v7a$|x86_64$|x86$|arm64-v8a$)',l[1])
-	architecture = name[1]
-	libraryName = name[0].replace("-"," ").title()
-	sizes = round(l[0]/(1024*1024),1)
 	if architecture == "x86":
 		archMaps[libraryName].x86 = sizes
 	elif architecture == "x86_64":
@@ -59,16 +69,24 @@ for line in text.splitlines():
 	else:
 		print("Missing arch {} is size {}".format(architecture, sizes))
 
+def sizeof_fmt(num, suffix="B"):
+    for unit in ("", "K", "M", "G", "T", "P", "E", "Z"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
 for a in archMaps:
 	if(a == "Without Sdk"):
 		continue
 	s = archMaps[a]
 	print("|  Phone Architecture     	|  "+ s.name + "   |")
 	print("| --------------------------------- | ---------  |")
-	print("| (armeabi-v7)                      | **{}MB**  |".format(s.armeabiv7a))
-	print("| (arm64-v8a) **(most common one)** | **{}MB**  |".format(s.arm64v8a))
-	print("| (x86_64)                          | **{}MB**  |".format(s.x8664))
-	print("| (x86)                             | **{}MB**  |".format(s.x86))
+	print("| (armeabi-v7)                      | **{}**  |".format(sizeof_fmt((s.armeabiv7a - subtractBy(s.name, "armeabi-v7a")))))
+	print("| (arm64-v8a) **(most common one)** | **{}**  |".format(sizeof_fmt((s.arm64v8a - subtractBy(s.name, "arm64-v8a")))))
+	print("| (x86_64)                          | **{}**  |".format(sizeof_fmt((s.x8664 - subtractBy(s.name, "x86_64")))))
+	print("| (x86)                             | **{}**  |".format(sizeof_fmt((s.x86 - subtractBy(s.name, "x86")))))
 	print()
 	print()
 
